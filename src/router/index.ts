328329d22742303a188f033c9fe513b5e1a756e6
MariuszMalankiewicz <m2ario123@wp.pl>
2025-01-14 13:09:24 +0100

    add check if user is logged in

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginPage from '../views/LoginPage.vue'
+import WeatherPage from '../views/WeatherPage.vue'
 
-const routes = [{ path: '/', component: LoginPage }]
+const routes = [
+  { path: '/', component: LoginPage },
+  {
+    path: '/weather',
+    component: WeatherPage,
+    beforeEnter: (to, from, next) => {
+      const user = localStorage.getItem('user')
+      if (!user) {
+        next('/')
+      } else {
+        next()
+      }
+    },
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
