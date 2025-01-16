a6a8179cc68cb7adcfa46c3e1e7b118a074f3d0f
MariuszMalankiewicz <m2ario123@wp.pl>
2025-01-16 12:55:19 +0100

    add TopHeader components

---
+++ src/components/TopHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="d-flex align-items-start justify-content-between mb-5">
+    <div class="d-flex">
+      <p class="me-3">Notification<span class="bg-danger rounded px-2 m-1">2</span></p>
+      <p>Places</p>
+    </div>
+    <div class="position-relative">
+      <div
+        class="rounded position-absolute top-0 start-50"
+        style="width: 6px; height: 6px; background-color: red"
+      ></div>
+      <img :src="awatar" alt="awatar" class="rounded me-4" style="width: 32px" />
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import awatar from '../assets/awatar.jpeg'
+export default {
+  name: 'TopHeader',
+  data() {
+    return {
+      awatar,
+    }
+  },
+}
+</script>
